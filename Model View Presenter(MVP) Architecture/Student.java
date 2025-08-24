643b8432825873e90b20db87e121c77618855509
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-08-24 23:08:35 +0600

    Create Student.java

---
+++ Model View Presenter(MVP) Architecture/Student.java
@@ -0,0 +1,19 @@
+//Model
+public class Student
+{
+    private  String name;
+    private  int age;
+    public  Student(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public  int getAge()
+    {
+        return age;
+    }
+    public  String getName()
+    {
+        return name;
+    }
+}
