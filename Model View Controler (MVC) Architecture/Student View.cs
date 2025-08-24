3a10ebb1f250b4734a37102aa50f186115b90a88
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-08-24 21:44:32 +0600

    Create Student View.cs

---
+++ Model View Controler (MVC) Architecture/Student View.cs
@@ -0,0 +1,12 @@
+public class StudentView
+{
+    public void printStudentDetails(string name,int age)
+    {
+        Console.WriteLine($"Student Name:{name}");
+        Console.WriteLine($"Student Age:{age}");
+    }
+    public void printMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
